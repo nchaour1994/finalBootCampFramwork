64055e86fa2417c7f56df22d991ea229d67c93cc
nabil <nchaour6@gmail.com>
2022-02-19 10:26:42 -0500

    modifications

--- Walgreens/src/test/java/org/piit/updateAccountInfo/UpdateAccountInformations.java
+++ Walgreens/src/test/java/org/piit/updateAccountInfo/UpdateAccountInformations.java
@@ -12,8 +12,7 @@ public class UpdateAccountInformations extends commonApi {
 
 
     @Test
-    public void updateMyAccountInformations(){
-       // HomePage home= PageFactory.initElements(driver, HomePage.class);
+    public void TestupdateMyAccountInformations(){
         HomePage home=new HomePage(driver);
         SignInPage signInPage=new SignInPage(driver);
         YourAccountPage yourAccountPage=new YourAccountPage(driver);
@@ -21,29 +20,30 @@ public class UpdateAccountInformations extends commonApi {
 
         Assert.assertTrue(home.accountBtn.isEnabled());
         home.clickOnAccountBtn();
-        waitFor(3);
+
         Assert.assertTrue(home.signInBtnInAccountMenu.isEnabled());
         home.clickOnsignInBtnInAccountMenu();
-        waitFor(3);
+
         signInPage.typeOnuserNameFieldInSignIn();
         signInPage.typeOnpasswordFieldInSignIn();
         Assert.assertTrue(signInPage.signInBtn.isEnabled());
         signInPage.clickOnsignInBtn();
-        waitFor(8);
+
         Assert.assertTrue(home.accountBtn.isEnabled());
         home.clickOnAccountBtn();
-        waitFor(2);
+
         Assert.assertTrue(home.accountHomeInAccountList.isEnabled());
         home.clickOnaccountHomeInAccountList();
         Assert.assertTrue(yourAccountPage.profileBtn.isDisplayed());
         yourAccountPage.hoverOverOnprofileBtn();
-        waitFor(5);
+        waitFor(1);
+
         Assert.assertTrue(yourAccountPage.personalInformationInProfile.isDisplayed());
         yourAccountPage.clickOnpersonalInformationInProfile();
-        waitFor(5);
+
         Assert.assertTrue(personalInformationPage.editBtn.isEnabled());
         personalInformationPage.clickOneditBtn();
-        waitFor(5);
+
         personalInformationPage.typeOnadressFieldInMyAccount();
 
     }
