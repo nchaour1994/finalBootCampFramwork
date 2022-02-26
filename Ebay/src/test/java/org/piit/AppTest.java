433fcee278f89abd98423adf414ca9301d64c2d6
nabil <nchaour6@gmail.com>
2022-02-25 21:32:51 -0500

    resolve path issue

--- Ebay/src/test/java/org/piit/AppTest.java
+++ Ebay/src/test/java/org/piit/AppTest.java
@@ -258,15 +258,4 @@ public class AppTest  extends commonApi {
 
 
 
-import base.commonApi;
-import org.testng.annotations.Test;
-
-public class AppTest  extends commonApi
-{
- @Test
- public void test01(){
-  waitFor(3);
- }
-
-}
 
